d20410b6f4fb03e83e7b8fb318b03ee208f047b3
Ian Spence <ian@ecnepsnai.com>
2020-11-10 19:03:57 -0800

    Fix typos in documentation

--- hash.go
+++ hash.go
@@ -19,7 +19,7 @@ func HashSHA256(raw []byte) []byte {
 	return hashWith(raw, sha256.New())
 }
 
-// HashSHA256String return a hexedacimal string representing the SHA-256 hash of the provided string. Do not use for passwords.
+// HashSHA256String return a hexadecimal string representing the SHA-256 hash of the provided string. Do not use for passwords.
 func HashSHA256String(raw string) string {
 	return hashWithString(raw, sha256.New())
 }
@@ -29,7 +29,7 @@ func HashSHA512(raw []byte) []byte {
 	return hashWith(raw, sha512.New())
 }
 
-// HashSHA512String return a hexedacimal string representing the SHA-512 hash of the provided string. Do not use for passwords.
+// HashSHA512String return a hexadecimal string representing the SHA-512 hash of the provided string. Do not use for passwords.
 func HashSHA512String(raw string) string {
 	return hashWithString(raw, sha512.New())
 }
@@ -43,7 +43,7 @@ func hashWithString(raw string, hasher hash.Hash) string {
 	return hex.EncodeToString(hashWith([]byte(raw), hasher))
 }
 
-// PassphraseToEncryptionKey hash a string sutible for a AES-256 key (32 bytes) using scrypt
+// PassphraseToEncryptionKey hash a string suitable for a AES-256 key (32 bytes) using scrypt
 func PassphraseToEncryptionKey(raw string) []byte {
 	key, _ := scrypt.Key([]byte(raw), nil, 32768, 8, 1, 32)
 	return key
--- random.go
+++ random.go
@@ -6,7 +6,7 @@ import (
 	mrand "math/rand"
 )
 
-// RandomBytes generate random bytes of specified length. Sutible for cryptographical use.
+// RandomBytes generate random bytes of specified length. suitable for cryptographical use.
 func RandomBytes(length uint16) []byte {
 	randB := make([]byte, length)
 	rand.Read(randB)
@@ -14,13 +14,14 @@ func RandomBytes(length uint16) []byte {
 }
 
 // RandomNumber generate a random number within the specified range.
-// Not sutible for cryptogrpahical use.
+// Not suitable for cryptographically use.
 func RandomNumber(min int, max int) int {
 	return mrand.Intn(max-min) + min
 }
 
-// RandomString generate a random string (hex characters) with the length of random entropy. Returned string will be around 2* longer than `length`.
-// Sutible for cryptographical use.
-func RandomString(length uint16) string {
-	return hex.EncodeToString(RandomBytes(length))
+// RandomString generate a random string (hex characters) with the length of random entropy.
+// Returned string will be exactly 2* longer than `randomLength`. For example, if you want a string that's 32 characters long, specify 16 for the random length.
+// Suitable for cryptographical use.
+func RandomString(randomLength uint16) string {
+	return hex.EncodeToString(RandomBytes(randomLength))
 }
